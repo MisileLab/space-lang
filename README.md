535cfb868c754047f7fcafaf62a01e930e24de62
misilelaboratory <misileminecord@gmail.com>
2023-01-08 18:16:49 +0900

    [ci skip] temp

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
+
 # Space lang
+
 ![Rust](https://img.shields.io/badge/language-rust-1976d2?style=for-the-badge&logo=rust)
 ![License](https://img.shields.io/badge/license-misilelab-green?style=for-the-badge)  
 [![Project Status](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
 
 WIP!!!
 
-Compiled, Functional programming language.
+Intepreted, Functional programming language.
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
-use std::env::args;
+use std::{env::args, any::Any, collections::HashMap};
 use tokio::fs::read_to_string;
 
 #[derive(Parser)]
@@ -14,10 +14,23 @@ struct IdentParser;
 async fn main() {
   let content = read_to_string(args().nth(1).unwrap()).await.unwrap();
   let pairs = IdentParser::parse(Rule::file, &content).unwrap_or_else(|e| panic!("{}", e)); 
-  for pair in pairs {
+  let functions: HashMap<String, dyn Any + 'Sized> = HashMap::new();
+  let variables: HashMap<String, dyn Any> = HashMap::new();
+
+  for pair in pairs.clone() {
     println!("Rule:{:?}", pair.as_rule());
     println!("Span:{:?}", pair.as_span());
     println!("Text:{}", pair.as_str());
+  }
+
+  for pair in pairs {
+    match pair.as_rule() {
+      Rule::file => {},
+      Rule::comment => {},
+      Rule::function => {
+        
+      }
+    }
   }
 }
 
